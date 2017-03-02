f7066d8f6176708b3152d5dfb845e3de95d3dd4d
Parmar <ashwin.d.parmar@accenture.com>
2017-03-03 01:02:29 +0530

    Java 8 Primitive Data Types, How to use Variables

---
+++ src/com/ashwin/UsingVariablePrimitiveDataType.java
@@ -0,0 +1,51 @@
+package com.ashwin;
+
+public class UsingVariablePrimitiveDataType {
+
+	/**
+	 * Java Using Variable
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		// 8 Primitive Data Type in Java
+		
+		// Declare Variable
+			int myNumber;
+			short myShortNum;
+			long myLong;
+			
+			double myDouble;
+			float myFloat;
+			
+			char myChar;
+			boolean myBoolean;
+			
+			byte myByte;
+		
+		// Initialize Variable
+			myNumber = 2017;
+			myShortNum = 123;
+			myLong = 9797;
+			
+			myDouble = 7.123123;
+			myFloat = 324.3f;
+			
+			myChar = 'A';
+			myBoolean = true;
+			
+			myByte = 127;
+					
+		// Print Variable
+			System.out.println("My Number is: " + myNumber);
+			System.out.println("My Short Number is: " + myShortNum);
+			System.out.println("My Long Number is: " + myLong);
+			System.out.println("My Double Number is: " + myDouble);
+			System.out.println("My Float Number is: " + myFloat);
+			System.out.println("My Char is: " + myChar);
+			System.out.println("My Boolean is: " + myBoolean);
+			System.out.println("My Byte is: " + myByte);
+
+	}
+
+}
