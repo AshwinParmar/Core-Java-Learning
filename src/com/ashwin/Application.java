cdccc7b6c08397a10df76cf2c1409527fd345cdc
Parmar <ashwin.d.parmar@accenture.com>
2017-03-03 00:48:14 +0530

    Hello World Program added

---
+++ src/com/ashwin/Application.java
@@ -0,0 +1,13 @@
+package com.ashwin;
+
+public class Application {
+
+	/**
+	 * @author Ashwin Parmar
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
