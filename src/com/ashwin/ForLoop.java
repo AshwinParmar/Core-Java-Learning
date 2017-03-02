379dfe33dbf8f31efa2948fa1c29d035b1f89abd
Parmar <ashwin.d.parmar@accenture.com>
2017-03-03 01:29:15 +0530

    Java For Loop Demo

---
+++ src/com/ashwin/ForLoop.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.ashwin;
+
+/**
+ * @author ashwin.d.parmar
+ *
+ */
+public class ForLoop {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("Hello " + i);
+		}
+
+	}
+
+}
