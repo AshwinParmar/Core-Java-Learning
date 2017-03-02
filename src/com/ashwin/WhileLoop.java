e2c1a81dd527bfee2e3c57ece78dcae11e249834
Parmar <ashwin.d.parmar@accenture.com>
2017-03-03 01:28:09 +0530

    Java While Loop Demo

---
+++ src/com/ashwin/WhileLoop.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.ashwin;
+
+/**
+ * @author ashwin.d.parmar
+ *
+ */
+public class WhileLoop {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// While Loop Demo
+		
+		int value = 1;
+		
+		while (value <= 10) {
+			System.out.println("Hello " + value);
+			value++;
+		}
+
+	}
+
+}
